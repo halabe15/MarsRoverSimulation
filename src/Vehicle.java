6fbe639acb0007d7b5f88288f503e7a629ef62ca
Ben Lawton <thisisbenlawton@gmail.com>
2017-11-10 17:00:56 +0000

    Implement Vehicle#actSimple()

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
 
-class Vehicle extends Entity{
+class Vehicle extends Entity {
 	public boolean carryingSample;
 	
-	public Vehicle(Location l){
+	public Vehicle(Location l) {
 		super(l);	
 		this.carryingSample = false;
 	}
 
 	public void act(Field f, Mothership m, ArrayList<Rock> rocksCollected)
 	{
-		actCollaborative(f,m,rocksCollected);
-		//actSimple(f,m,rocksCollected);
+		//actCollaborative(f,m,rocksCollected);
+		actSimple(f,m,rocksCollected);
 	}
 	
 	
@@ -20,6 +20,41 @@ class Vehicle extends Entity{
 	}
 
 	public void actSimple(Field f, Mothership m, ArrayList<Rock> rocksCollected){
-		//complete this method
+		Location base = f.getNeighbour(location, Mothership.class);
+		boolean atBase = (base != null) ? true : false;
+		ArrayList<Location> adjLocations = f.getAllfreeAdjacentLocations(location);
+		Location neighbouringSample = f.getNeighbour(location, Rock.class);
+		
+		
+		// If carrying sample and at base, drop sample 
+		if (carryingSample && atBase) {
+			carryingSample = false;
+		} 
+		// If carrying sample and not at base, travel up gradient  
+		else if (carryingSample && !atBase) {
+			for (Location adjLoc : adjLocations) {
+				if (f.getSignalStrength(adjLoc) > f.getSignalStrength(location)) {
+					f.clearLocation(location);
+					f.place(this, adjLoc);
+					setLocation(adjLoc);
+					return;
+				}
+			} 
+		} 
+		// If a sample is detected, pick up sample 
+		else if (neighbouringSample != null) {
+			f.clearLocation(neighbouringSample);
+			carryingSample = true;
+		} 
+		// If nothing else to do, move randomly 
+		else {
+			Location neighbour = f.freeAdjacentLocation(location);
+			if (neighbour != null) {
+				f.clearLocation(location);
+				f.place(this, neighbour);
+				setLocation(neighbour);
+				return;
+			}
+		}
 	}
 }
